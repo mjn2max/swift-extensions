6f22901c2ed2a57d22e17518fbdb63521f80e801
Huy D. <116730653+mjn2max@users.noreply.github.com>
2025-06-16 22:27:59 -0400

    Update Double extension

--- Sources/Double.swift
+++ Sources/Double.swift
@@ -4,11 +4,28 @@
 //
 // Created by Huy D. on 10/14/24
 // mjn2max.github.io 😜
-// 
+//
 // Copyright © 2024. All rights reserved.
 // CodePassion.dev
 //
 
+import Foundation
+
+// MARK: - Double Extensions - Bool
+extension Double {
+    /// Returns true if the value has no fractional component (i.e., is mathematically an integer).
+    ///
+    /// Usage:
+    /// ```
+    /// let num = 5.0
+    /// let isInt = num.isInteger // true
+    /// ```
+    var isInteger: Bool {
+        truncatingRemainder(dividingBy: 1) == 0
+    }
+}
+
+// MARK: - Double Extensions - Temperature
 extension Double {
     func celsiusToFahrenheit() -> Double {
         return self * 9 / 5 + 32
