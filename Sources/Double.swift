a2d8895140692e082bad04a70f9ddb277216ba3c
Huy D. <116730653+mjn2max@users.noreply.github.com>
2025-06-16 22:39:00 -0400

    Update Double extension

--- Sources/Double.swift
+++ Sources/Double.swift
@@ -11,9 +11,9 @@
 
 import Foundation
 
-// MARK: - Double Extensions - Bool
-extension Double {
-    /// Returns true if the value has no fractional component (i.e., is mathematically an integer).
+// MARK: - Double Extensions - Return `Bool`
+public extension Double {
+    /// Returns `true` if the value has no fractional component (i.e., is mathematically an integer).
     ///
     /// Usage:
     /// ```
@@ -25,8 +25,49 @@ extension Double {
     }
 }
 
+// MARK: - Double Extensions - Return `Double`
+
+public extension Double {
+    /// Returns the value rounded to a specified number of decimal places.
+    ///
+    /// - Parameter places: Number of decimal places to round to.
+    /// - Returns: Rounded Double.
+    ///
+    /// Usage:
+    /// ```
+    /// let value = 3.14159
+    /// let rounded = value.rounded(toPlaces: 2) // 3.14
+    /// ```
+    func rounded(toPlaces places: Int) -> Double {
+        guard places >= 0 else { return self }
+        let factor = pow(10.0, Double(places))
+        return (self * factor).rounded() / factor
+    }
+
+
+    /// Converts degrees to radians.
+    ///
+    /// Usage:
+    /// ```
+    /// let degrees = 180.0
+    /// let radians = degrees.degreesToRadians // 3.141592653589793
+    /// ```
+    var degreesToRadians: Double { self * .pi / 180 }
+
+
+    /// Converts radians to degrees.
+    ///
+    /// Usage:
+    /// ```
+    /// let radians = Double.pi
+    /// let degrees = radians.radiansToDegrees // 180.0
+    /// ```
+    var radiansToDegrees: Double { self * 180 / .pi }
+}
+
 // MARK: - Double Extensions - Temperature
-extension Double {
+
+public extension Double {
     func celsiusToFahrenheit() -> Double {
         return self * 9 / 5 + 32
     }
