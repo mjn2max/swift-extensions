605cc14669626792f8561abe8ddbed120d72dde8
Huy D. <116730653+mjn2max@users.noreply.github.com>
2025-06-26 08:19:54 -0400

    Decimal | Add `var isInteger: Bool`

--- Sources/Numbers/Decimal.swift
+++ Sources/Numbers/Decimal.swift
@@ -4,14 +4,14 @@
 //
 // Created by Huy D. on 6/24/25
 // mjn2max.github.io 😜
-// 
+//
 // Copyright © 2025. All rights reserved.
 // CodePassion.dev
 //
 
 import Foundation
 
-// MARK: - Decimal Extensions
+// MARK: - Decimal Extensions - Return `Decimal`
 
 extension Decimal {
     /// Returns the value rounded to a specified number of decimal places.
@@ -29,5 +29,25 @@ extension Decimal {
         var rounded = Decimal()
         NSDecimalRound(&rounded, &result, places, .plain)
         return rounded
+    }
+}
+
+// MARK: - Decimal Extensions - Return `Bool`
+
+extension Decimal {
+    /// Returns `true` if the decimal value is an integer (has no fractional part).
+    ///
+    /// - Returns: `true` if the value is an integer, otherwise `false`.
+    ///
+    /// # Usage
+    /// ```
+    /// let whole: Decimal = 10
+    /// let isInt = whole.isInteger // true
+    ///
+    /// let pi: Decimal = 3.14
+    /// let isPiInt = pi.isInteger // false
+    /// ```
+    var isInteger: Bool {
+        NSDecimalNumber(decimal: self).decimalValue == NSDecimalNumber(decimal: self).rounding(accordingToBehavior: nil).decimalValue
     }
 }
